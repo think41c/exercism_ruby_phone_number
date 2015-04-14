4efc1b006220b9fb2c09765155c9ae6cb19509be
Tony DiNitto <think41c@gmail.com>
2015-04-14 12:44:41 -0600

    Basic cleaning logic started with .gsub

--- phone_number.rb
+++ phone_number.rb
@@ -1,12 +1,18 @@
 class PhoneNumber
   def initialize(phone)
+    @phone = phone
   end
 
     
   def number
+    cleaned_num = @phone.gsub(/[()]/,"")
     "1234567890"
+    cleaned_num
   end
 
   def area_code
   end
 end
+
+a = PhoneNumber.new("()123")
+p a.number
