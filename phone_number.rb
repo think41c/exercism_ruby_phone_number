5f47aa22467a75cf6d5d46f6bbbac30b7776c0b7
Tony DiNitto <think41c@gmail.com>
2015-04-14 13:34:14 -0600

    Extracted more methods to smaller responsibility.

--- phone_number.rb
+++ phone_number.rb
@@ -3,21 +3,32 @@ class PhoneNumber
     @phone = phone
   end
 
-    
-  def number
+  def delete_the_one
+    if @phone[0] == "1" 
+      @phone[0] = ""
+    end
+  end
+
+  def reject_letters
     if @phone.each_char.any? do |x|
         x =~ /[A-Z]/i
       end
       "0000000000"
-    else
-      cleaned_num = @phone.gsub(/[\D]/,"")
-      cleaned_num
     end
+  end
+
+    
+  def number
+    delete_the_one
+    reject_letters 
+
+    cleaned_num = @phone.gsub(/[\D]/,"")
+    cleaned_num
   end
 
   def area_code
   end
 end
 
-a = PhoneNumber.new("2345")
+a = PhoneNumber.new("1234---2345")
 p a.number
