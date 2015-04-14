f1a0502c14666eb42ec98f94144d6c972b277688
Tony DiNitto <think41c@gmail.com>
2015-04-14 13:37:46 -0600

    Finish method splitting.

--- phone_number.rb
+++ phone_number.rb
@@ -13,22 +13,33 @@ class PhoneNumber
     if @phone.each_char.any? do |x|
         x =~ /[A-Z]/i
       end
+      @phone = "0000000000"
+      @phone
+    end
+  end
+
+  def clean_chars 
+    @phone = @phone.gsub(/[\D]/,"")   
+  end
+
+  def check_length
+    if @phone.length < 20
+      @phone
+    else
       "0000000000"
     end
   end
 
-    
   def number
     delete_the_one
     reject_letters 
-
-    cleaned_num = @phone.gsub(/[\D]/,"")
-    cleaned_num
+    clean_chars
+    check_length
   end
 
   def area_code
   end
 end
 
-a = PhoneNumber.new("1234---2345")
+a = PhoneNumber.new("12334525")
 p a.number
